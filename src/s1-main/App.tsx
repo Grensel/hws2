9a72ce6bdd3af0e1ec16f0dbba4f380765ac4ce3
Grensel <sergeiodinochenko@mail.ru>
2025-02-07 18:28:33 +0300

    feat: completed second hw task

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -1,12 +1,13 @@
 import s from "./App.module.css";
 import HW1 from "../s2-homeworks/hw01/HW1";
+import HW2 from "../s2-homeworks/hw02/HW2";
 
 function App() {
   return (
     <div className={s.App}>
       <HW1 />
-      {/*раскомментировать по ходу выполнения*/}
-      {/*<HW2/>*/}
+      {/* раскомментировать по ходу выполнения */}
+      <HW2 />
       {/*<HW3/>*/}
       {/*<HW4/>*/}
 
@@ -17,3 +18,4 @@ function App() {
 }
 
 export default App;
+
--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -70,3 +70,4 @@ const HW1 = () => {
 };
 
 export default HW1;
+
