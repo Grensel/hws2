4aa78c91338f3dc972153bf4bcd87a4a524886b0
Grensel <sergeiodinochenko@mail.ru>
2025-03-04 15:34:43 +0300

    feat: completed hw6

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -19,7 +19,8 @@ const HW6 = () => {
     }
     const restore = () => {
         // делают студенты
-
+        const newStore = restoreState('hw6-editable-span-value', '')
+        setValue(newStore)
     }
 
     return (
@@ -35,7 +36,7 @@ const HW6 = () => {
                         onChangeText={setValue}
                         spanProps={{
                             id: 'hw6-editable-span',
-                            defaultText: 'enter text...',
+                            defaultText: 'Enter text...',
                         }}
                     />
                 </div>
